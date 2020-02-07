2aa4d8e1a27b77270c1d81ec88f45eba45c5d8e1
BarriF13 <info@bdesignstudio.co.uk>
2020-02-07 16:51:54 +0000

    Multiplayer S01

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -5,4 +5,9 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content 'Barri: 60HP'
   end
+
+  scenario 'see player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Myrtle: 60HP'
+  end
 end
